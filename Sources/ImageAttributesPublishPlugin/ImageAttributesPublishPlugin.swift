e095f6b8fff365e1ffdf311971ba5a4e7d956975
Sven A. Schmidt <sas@finestructure.co>
2020-02-29 13:48:57 +0100

    Correctly handle existing title

--- Sources/ImageAttributesPublishPlugin/ImageAttributesPublishPlugin.swift
+++ Sources/ImageAttributesPublishPlugin/ImageAttributesPublishPlugin.swift
@@ -3,9 +3,48 @@ import Ink
 import Publish
 
 
+enum Delimiter {
+    case squareBrackets
+    case parentheses
+
+    var opening: Character {
+        switch self {
+            case .squareBrackets: return "["
+            case .parentheses: return "("
+        }
+    }
+
+    var closing: Character {
+        switch self {
+            case .squareBrackets: return "]"
+            case .parentheses: return ")"
+        }
+    }
+}
+
+extension Substring {
+    // Naive content delimination, in the sense that it does not
+    // count multiple levels of opening delimiters.
+    // Simply returns string between first occurrences of opening
+    // and closing delimiters.
+    func content(delimitedBy delimiter: Delimiter) -> Substring? {
+        guard let delimPos = self.firstIndex(of: delimiter.opening) else { return nil }
+        let start = self.index(after: delimPos)
+        guard let end = self.firstIndex(of: delimiter.closing) else { return nil }
+        guard start <= end else { return nil }
+        return self[start..<end]
+    }
+}
+
+
 struct Attribute {
     var key: String
     var value: String
+
+    init(key: String, value: String) {
+        self.key = key
+        self.value = value
+    }
 
     init?(_ rawValue: String) {
         let pair = rawValue.split(separator: "=").map(String.init)
@@ -32,20 +71,20 @@ public extension Plugin {
 public extension Modifier {
     static func imageAttributes() -> Self {
         return Modifier(target: .images) { html, markdown in
-            guard
-                let paren = markdown.firstIndex(of: "("),
-                markdown.last == ")" else { return html }
-
-            let start = markdown.index(after: paren)
-            let link = markdown[start...].dropLast()
+            guard let title = markdown.content(delimitedBy: .squareBrackets) else { return html }
+            guard let link = markdown.content(delimitedBy: .parentheses) else { return html }
             let parts = link.components(separatedBy: CharacterSet.whitespaces)
 
             guard let url = parts.first else { return html }
 
             guard parts.count > 1 else { return html }
-            let attributes = parts
+            var attributes = parts
                 .dropFirst()
                 .compactMap(Attribute.init)
+            if !title.isEmpty {
+                attributes.append(Attribute(key: "alt", value: String(title)))
+            }
+            attributes.sort(by: { $0.key < $1.key })
 
             guard !attributes.isEmpty else { return html }
             let attrs = attributes
--- Tests/ImageAttributesPublishPluginTests/ImageAttributesPublishPluginTests.swift
+++ Tests/ImageAttributesPublishPluginTests/ImageAttributesPublishPluginTests.swift
@@ -1,8 +1,21 @@
-import ImageAttributesPublishPlugin
+@testable import ImageAttributesPublishPlugin
 import Ink
 import XCTest
 
+
 final class ImageAttributesPublishPluginTests: XCTestCase {
+
+    func test_content_delimitedBy() throws {
+        XCTAssertEqual("foo [bar] baz".content(delimitedBy: .squareBrackets), "bar")
+        XCTAssertEqual("foo ]bar] baz".content(delimitedBy: .squareBrackets), nil)
+        XCTAssertEqual("foo ]bar[ baz".content(delimitedBy: .squareBrackets), nil)
+        XCTAssertEqual("foo (bar) baz".content(delimitedBy: .parentheses), "bar")
+        XCTAssertEqual("foo (bar[) baz".content(delimitedBy: .parentheses), "bar[")
+        XCTAssertEqual("foo (b[ar]) baz".content(delimitedBy: .parentheses), "b[ar]")
+        XCTAssertEqual("foo (".content(delimitedBy: .parentheses), nil)
+        XCTAssertEqual("foo ()".content(delimitedBy: .parentheses), "")
+        XCTAssertEqual("foo )".content(delimitedBy: .parentheses), nil)
+    }
 
     func test_parse_single_attribute() throws {
         let parser = MarkdownParser(modifiers: [.imageAttributes()])
@@ -10,7 +23,28 @@ final class ImageAttributesPublishPluginTests: XCTestCase {
         XCTAssertEqual(html, #"<img src="https://example/image.png" width="400"/>"#)
     }
 
+    func test_parse_single_attribute_with_link_title() throws {
+        let parser = MarkdownParser(modifiers: [.imageAttributes()])
+        let html = parser.html(from: "![title](https://example/image.png width=400)")
+        XCTAssertEqual(html, #"<img src="https://example/image.png" alt="title" width="400"/>"#)
+    }
+
+    func test_parse_single_attribute_with_link_title() throws {
+        let parser = MarkdownParser(modifiers: [.imageAttributes()])
+        let html = parser.html(from: "![title](https://example/image.png width=400)")
+        XCTAssertEqual(html, #"<img src="https://example/image.png" alt="title" width="400"/>"#)
+    }
+
     static var allTests = [
         ("test_parse_single_attribute", test_parse_single_attribute),
     ]
+}
+
+
+extension String {
+    // Test convenience so we can test with String rather than have to construct
+    // substrings
+    func content(delimitedBy delimiter: Delimiter) -> String? {
+        (self[...] as Substring).content(delimitedBy: delimiter).map(String.init)
+    }
 }
